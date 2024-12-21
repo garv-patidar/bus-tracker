20012ab463c46927b60dfdb19c9cb2bbbfc52f29
Garv Patidar <ee3231206@ee.iitd.ac.in>
2024-12-21 20:32:28 +0530

    BusTrackerWithOTA.ino

--- Arduino Code/XiaoESP32FinalWorkingCodeWithWifiReconnectBest/XiaoESP32FinalWorkingCodeWithWifiReconnectBest.ino
+++ Arduino Code/XiaoESP32FinalWorkingCodeWithWifiReconnectBest/XiaoESP32FinalWorkingCodeWithWifiReconnectBest.ino
@@ -1,12 +1,19 @@
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <NetworkUdp.h>
+#include <ArduinoOTA.h>
 #include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
+#include "config.h"
+const char *ssid = "";
+const char *password = "";
 static const int RXPin = D7, TXPin = D6;
 static const uint32_t GPSBaud = 9600;
 TinyGPSPlus gps;
 SoftwareSerial ss(RXPin, TXPin);
 
 #include "config.h"  // Configuration for Adafruit IO
-
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 int value = 0;
 double lat = 28.545684;
 double lon = 77.185579;
@@ -19,25 +26,85 @@ AdafruitIO_Feed *latitudeFeed = io.feed("latitude");
 
 void setup() {
   Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  Serial.begin(115200);
   ss.begin(GPSBaud);
 
   Serial.print("Connecting to Adafruit IO");
   io.connect();
-
-  // latitudeFeed->onMessage(handleMessage);
-
-  // wait for a connection
   while(io.status() < AIO_CONNECTED) {
     Serial.print(".");
     delay(500);
-  }
 
   Serial.println();
   Serial.println(io.statusText());
   latitudeFeed->get();
+  }
+
+
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  // ArduinoOTA.setHostname("myesp32");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH) {
+        type = "sketch";
+      } else {  // U_SPIFFS
+        type = "filesystem";
+      }
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) {
+        Serial.println("Auth Failed");
+      } else if (error == OTA_BEGIN_ERROR) {
+        Serial.println("Begin Failed");
+      } else if (error == OTA_CONNECT_ERROR) {
+        Serial.println("Connect Failed");
+      } else if (error == OTA_RECEIVE_ERROR) {
+        Serial.println("Receive Failed");
+      } else if (error == OTA_END_ERROR) {
+        Serial.println("End Failed");
+      }
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
+  ArduinoOTA.handle();
 
   // Check if the Adafruit IO connection is lost and reconnect
   if(io.status() != AIO_CONNECTED) {
@@ -124,4 +191,3 @@ static void printFloat(float val, bool valid, int len, int prec)
       gps.encode(ss.read());
   } while (millis() - start < ms);
 }
-
